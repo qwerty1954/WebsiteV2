bd8a36f382ebacae5274a1873c319a79217bc378
Cloud Kanou <cloud.kanou@gmail.com>
2020-01-25 13:20:28 -0800

    Revert header styling to fix bugs

--- src/app/app-core/app-core-routing.module.ts
+++ src/app/app-core/app-core-routing.module.ts
@@ -4,13 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ConnectFourComponent } from './../connect-four/connect-four.component';
 import { TicTacToeComponent } from './../tic-tac-toe/tic-tac-toe.component';
 import { BeatSaberComponent } from './../beat-saber/beat-saber.component';
-import { PageNotFoundComponent } from './../page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'connect-4', component: ConnectFourComponent},
   {path: 'tic-tac-toe', component: TicTacToeComponent},
-  {path: 'beat-saber', component: BeatSaberComponent},
-  {path: '**', component: PageNotFoundComponent},
+  {path: 'beat-saber', component: BeatSaberComponent}
 ]
 
 @NgModule({
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Custom Components
 import { IntroductionComponent } from './app-core/introduction/introduction.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ConnectFourComponent } from './connect-four/connect-four.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+import { BeatSaberComponent } from './beat-saber/beat-saber.component';
 
 const routes: Routes = [
-  {path: '', component: IntroductionComponent}
+  {path: 'connect-4', component: ConnectFourComponent},
+  {path: 'tic-tac-toe', component: TicTacToeComponent},
+  {path: 'beat-saber', component: BeatSaberComponent},
+  {path: '', component: IntroductionComponent},
+  {path: '**', component: PageNotFoundComponent}
 ]
 
 @NgModule({
