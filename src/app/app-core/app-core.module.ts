650987242fafab66d4450ee15dc44e5f8a4cb1e8
Cloud Kanou <cloud.kanou@gmail.com>
2020-01-25 12:08:26 -0800

    Refactor routing with AppCore Module

--- src/app/app-core/app-core.module.ts
+++ src/app/app-core/app-core.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Custom Components
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
-
+// Custom Modules
+import { AppCoreRoutingModule } from './app-core-routing.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidenavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppCoreRoutingModule
   ],
   exports: [
     HeaderComponent,
